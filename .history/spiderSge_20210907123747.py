91394d2f9ed42f636f7729efc17e8fe5c1606639
jeckun <sz.eric.yang@qq.com>
2021-09-07 12:40:45 +0800

    new spiderSge
    重写采集程序，为了更简洁的获取数据。

---
+++ .history/spiderSge_20210907123747.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+
+
+
+
+def main(args):
+    print('数据采集工具2.0')
+    print('数据来源：上海黄金交易所')
+    print('网络地址：https://www.sge.com.cn/')
+    print('采集最近%s的数据。') % args[0]
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ .history/spiderSge_20210907123851.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+
+
+
+
+def main(args):
+    print('\n数据采集工具2.0')
+    print('数据来源：上海黄金交易所')
+    print('网络地址：https://www.sge.com.cn/')
+    print('本次任务：采集最近%s的数据。' % args[0]) 
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
